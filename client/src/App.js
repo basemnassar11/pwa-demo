f389de3f40051e228763e7b2857d0c1e6869c628
Basem <basemnassar11@gmail.com>
2022-07-25 22:53:10 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,11 @@ function App() {
     const [activeId, setActiveId] = useState();
     const [hospitalData, setHospitalData] = useState({
         id: null,
-        name: '',
-        specialities: 0,
-        doctors: 0,
-        surgeries: 0,
-        bedCount: 0,
+        email: '',
+        first_name: '',
+        last_name: '',
     });
+    console.log(hospitalData)
     const [message, setMessage] = useState();
 
     useEffect(() => {
@@ -21,8 +20,9 @@ function App() {
 
     function fetchHospitals() {
         axios
-            .get('http://localhost:4000/hospitals')
+            .get('https://reqres.in/api/users')
             .then((response) => {
+              console.log(response.data.data); 
                 const dbOpenRequest = indexedDB.open('hospitalDB', 1);
                 dbOpenRequest.onupgradeneeded = function (event) {
                     const db = event.target.result;
@@ -34,12 +34,12 @@ function App() {
                     const store = txn.objectStore('hospitalStore');
                     const clearRequest = store.clear();
                     clearRequest.onsuccess = function () {
-                        response.data.forEach((hospital) => {
+                        response.data.data.forEach((hospital) => {
                             store.add(hospital);
                         });
                     };
                 };
-                setHospitals(response.data);
+                setHospitals(response.data.data);
             })
             .catch(() => {
                 const dbOpenRequest = indexedDB.open('hospitalDB', 1);
@@ -74,7 +74,7 @@ function App() {
     function handleSubmit(e) {
         e.preventDefault();
         axios
-            .post(`http://localhost:4000/hospital/${activeId}`, hospitalData)
+            .post(`https://reqres.in/api/users/${activeId}`, hospitalData)
             .then(() => {
                 setMessage('Data saved successfully');
                 setActiveId(null);
@@ -120,45 +120,27 @@ function App() {
                         key={hospital.id}
                         onSubmit={handleSubmit}
                     >
-                        <h3>{hospital.name}</h3>
+                        <h3>{hospital.email}</h3>
                         <div className="form__control">
-                            <label>Specialities</label>
+                            <label>first_name</label>
                             <input
-                                type="number"
-                                value={hospitalData.specialities}
+                                type="text"
+                                value={hospitalData.first_name}
                                 onChange={(e) =>
-                                    handleDataChange(e, 'specialities')
+                                    handleDataChange(e, 'first_name')
                                 }
                             />
                         </div>
                         <div className="form__control">
-                            <label>Doctors</label>
+                            <label>last_name</label>
                             <input
-                                type="number"
-                                value={hospitalData.doctors}
-                                onChange={(e) => handleDataChange(e, 'doctors')}
+                                type="text"
+                                value={hospitalData.last_name}
+                                onChange={(e) => handleDataChange(e, 'last_name')}
                             />
                         </div>
-                        <div className="form__control">
-                            <label>Surgeries</label>
-                            <input
-                                type="number"
-                                value={hospitalData.surgeries}
-                                onChange={(e) =>
-                                    handleDataChange(e, 'surgeries')
-                                }
-                            />
-                        </div>
-                        <div className="form__control">
-                            <label>Bed count</label>
-                            <input
-                                type="number"
-                                value={hospitalData.bedCount}
-                                onChange={(e) =>
-                                    handleDataChange(e, 'bedCount')
-                                }
-                            />
-                        </div>
+                       
+                       
                         <div className="buttons">
                             <button
                                 type="button"
@@ -172,11 +154,9 @@ function App() {
                     </form>
                 ) : (
                     <div key={hospital.id} className="card">
-                        <h3>{hospital.name}</h3>
-                        <div>Specialities: {hospital.specialities}</div>
-                        <div>Doctors: {hospital.doctors}</div>
-                        <div>Surgeries: {hospital.surgeries}</div>
-                        <div>Bed count: {hospital.bedCount}</div>
+                        <h3>{hospital.email}</h3>
+                        <div>first_name: {hospital.first_name}</div>
+                        <div>last_name: {hospital.last_name}</div>
                         <div className="buttons">
                             <button
                                 className="button"
